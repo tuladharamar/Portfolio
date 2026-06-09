55225b63959cba1bff6233442900bbfb40457c53
tuladharamar <tuladharamar@gmail.com>
2026-06-09 12:54:12 +0545

    minor bug in cors

--- Backend/server.js
+++ Backend/server.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(cors({
   origin: process.env.FRONTEND_URL || "*",
   methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
 
