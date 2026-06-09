3fe9e73da493720fc28d3c4aa0ed53efbd5a1e28
tuladharamar <tuladharamar@gmail.com>
2026-06-09 15:04:50 +0545

    cors fix

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,6 @@ app.use(express.json());
 app.use(cors({
   origin: process.env.FRONTEND_URL || "*",
   methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
 
